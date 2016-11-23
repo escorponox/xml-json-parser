4387efa8055ff4c86e7863a2122957ae112debd2
Carlos Coves Prieto <escorponox@gmail.com>
2016-11-23 14:22:56 +0100

    complex tests pass

--- app/scripts/xml-json-parser.js
+++ app/scripts/xml-json-parser.js
@@ -1,3 +1,61 @@
-const xmlJsonParser = () => '';
+const parseAttributes = (attr) => {
+  const attribute = /([^\s=]+)(="([^"]*)")?/g;
+  const result = {};
+  let execResult;
+  while ((execResult = attribute.exec(attr)) !== null) {
+    result['_attr_' + execResult[1]] = execResult[3] || true;
+  }
+  return result;
+};
+
+const countElements = (elements) => elements.reduce((prev, curr) => {
+  prev[curr.name] = ++prev[curr.name] || 1;
+  return prev;
+}, {});
+
+function parseElement(element) {
+  const parsedElement = parseAttributes(element.attributes);
+  if (typeof element.content === 'object') {
+    Object.assign(parsedElement, element.content);
+  } else if (Object.keys(parsedElement).length) {
+    if (element.content) parsedElement['_text_'] = element.content;
+  }
+  return Object.keys(parsedElement).length ? parsedElement : element.content;
+
+}
+const parseElements = (elements) => {
+
+  const elementCounter = countElements(elements);
+  const result = {};
+  elements.forEach((element) => {
+    if (elementCounter[element.name] > 1) {
+      if (!result.hasOwnProperty(element.name)) result[element.name] = [];
+      result[element.name].push(parseElement(element));
+    }
+    else {
+      result[element.name] = parseElement(element);
+    }
+  });
+  return result;
+};
+
+const xmlJsonParser = (xmlString = '') => {
+  const openingTag = /<([a-zA-z_][\w\-]*)([^\/>]*)(\/)?>/g;
+  const innerElements = [];
+  let execResult;
+  while ((execResult = openingTag.exec(xmlString)) !== null) {
+    const closingTag = `</${execResult[1]}>`;
+    const closingTagIndex = xmlString.indexOf(closingTag, openingTag.lastIndex);
+    innerElements.push({
+      name: execResult[1],
+      attributes: execResult[2],
+      content: execResult[3] === '\/' ? ''
+        : xmlJsonParser(xmlString.substring(execResult.index + execResult[0].length, closingTagIndex))
+    });
+    openingTag.lastIndex = execResult[3] === '\/' ? openingTag.lastIndex : closingTagIndex + closingTag.length;
+  }
+
+  return innerElements.length ? parseElements(innerElements) : xmlString;
+};
 
 export default xmlJsonParser;
--- test/scripts/xml-json-parser.test.js
+++ test/scripts/xml-json-parser.test.js
@@ -4,7 +4,116 @@
 import test from 'tape';
 import parser from '../../app/scripts/xml-json-parser';
 
-test('it runs', (t) => {
-  t.equal(parser(),'', 'No input returns empty string');
+test('root element', (t) => {
+  t.equal(parser(), '', 'No input returns ""');
+  t.equal(parser(''), '', 'Empty input returns ""');
+  t.deepEqual(parser('<files></files>'), {files: ""}, 'Empty root returns a empty value');
+  t.deepEqual(parser(`<files>
+</files>`), {files: '\n'}, 'Multiline elements');
+  t.deepEqual(parser('<files>lalala</files>'),
+    {
+      files: 'lalala'
+    },
+    'Text element returns the text');
+  t.deepEqual(parser('<files lalala></files>'),
+    {
+      files: {
+        _attr_lalala: true
+      }
+    },
+    'Empty root with empty attribute return only a true attribute');
+
+  t.deepEqual(parser('<files lalala>lelele</files>'),
+    {
+      files: {
+        _attr_lalala: true,
+        '_text_': 'lelele'
+      }
+    },
+    'Text root with attributes returns the attributes and the text');
+  t.end();
+});
+
+test('complex', (t) => {
+
+  t.deepEqual(parser('<files><file>a</file><file>b</file></files>'),
+    {
+      files: {
+        file: ['a', 'b']
+      }
+    },
+    'multiple inner tags of same type is an array');
+
+
+  t.deepEqual(parser(`<file lala="ajs">
+<title >Pandora</title>
+    <author  asdajh>Pedro Vian</author>
+    <album asdasd="34:45">Viejuno</album>
+    <genre>Electronic</genre>
+    <emptyTag />
+</file>`), {
+    file: {
+      _attr_lala: 'ajs',
+      title: 'Pandora',
+      author: {
+        _attr_asdajh: true,
+        _text_: 'Pedro Vian',
+      },
+      album: {
+        _attr_asdasd: '34:45',
+        _text_: 'Viejuno'
+      },
+      genre: 'Electronic',
+      emptyTag: ''
+    }
+  }, 'a complex audio file xml');
+
+  t.deepEqual(parser(`<files><file lala="ajs">
+<title >Pandora</title>
+    <author  asdajh>Pedro Vian</author>
+    <album asdasd="34:45">Viejuno</album>
+    <genre>Electronic</genre>
+    <emptyTag />
+</file>
+<file lala="ajsa">
+<title >Pandora</title>
+    <author  asdajh>Pedro Vian</author>
+    <album asdasd="34:45">Viejuno</album>
+    <genre>Electronic</genre>
+    <emptyTag />
+</file>
+</files>`), {
+    files: {
+      file: [{
+        _attr_lala: 'ajs',
+        title: 'Pandora',
+        author: {
+          _attr_asdajh: true,
+          _text_: 'Pedro Vian',
+        },
+        album: {
+          _attr_asdasd: '34:45',
+          _text_: 'Viejuno'
+        },
+        genre: 'Electronic',
+        emptyTag: ''
+      },
+        {
+          _attr_lala: 'ajsa',
+          title: 'Pandora',
+          author: {
+            _attr_asdajh: true,
+            _text_: 'Pedro Vian',
+          },
+          album: {
+            _attr_asdasd: '34:45',
+            _text_: 'Viejuno'
+          },
+          genre: 'Electronic',
+          emptyTag: ''
+        }]
+    }
+  }, 'a complex audio files xml');
+
   t.end();
 });
